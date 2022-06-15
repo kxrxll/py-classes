8da0e2a317ec97e0cf57e9ffb68756a385f14070
kxrxll <kovalyovkirill@gmail.com>
2022-06-15 15:15:43 +0300

    Done

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        return f"Name: {self.name}\nSurname: {self.surname}\nAverage grade: {self.average_grade()}\nCourses in " \
+               f"progress: {' '.join(self.courses_in_progress)}\nFinished courses: {' '.join(self.finished_courses)}\n"
+
     def rate_lc(self, lecturer, course, grade):
         if isinstance(lecturer,
                       Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
@@ -16,6 +20,15 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Error'
+
+    def average_grade(self):
+        grade_sum = 0
+        courses_number = 0
+        for course in self.grades:
+            courses_number += 1
+            result = sum(self.grades[course]) / len(self.grades[course])
+            grade_sum += result
+        return grade_sum / courses_number
 
 
 class Mentor:
@@ -30,8 +43,23 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        return f"Name: {self.name}\nSurname: {self.surname}\nAverage grade: {self.average_grade()}"
+
+    def average_grade(self):
+        grade_sum = 0
+        courses_number = 0
+        for course in self.grades:
+            courses_number += 1
+            result = sum(self.grades[course]) / len(self.grades[course])
+            grade_sum += result
+        return grade_sum / courses_number
+
 
 class Reviewer(Mentor):
+    def __str__(self):
+        return f"Name: {self.name}\nSurname: {self.surname}\n"
+
     def rate_hw(self, student, course, grade):
         if isinstance(student,
                       Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -45,6 +73,7 @@ class Reviewer(Mentor):
 
 best_student = Student('Ruby', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.finished_courses += ['Git']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
@@ -60,5 +89,6 @@ best_student.rate_lc(cool_lecturer, 'Python', 20)
 best_student.rate_lc(cool_lecturer, 'Python', 30)
 best_student.rate_lc(cool_lecturer, 'Python', 40)
 
-print(best_student.grades)
-print(cool_lecturer.grades)
+print(best_student)
+print(cool_reviewer)
+print(cool_lecturer)
